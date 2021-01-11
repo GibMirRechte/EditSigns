fe730fe449fe74a462cc4be4f92b5b1c66890ba3
GibMirRechte <55894240+DiesesForum@users.noreply.github.com>
2021-01-11 22:41:04 +0100

    Added version 1.0.6
    [+] Fixed a bug

--- src/de/editsign/main/Main.java
+++ src/de/editsign/main/Main.java
@@ -1,5 +1,6 @@
 package de.editsign.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -14,7 +15,7 @@ public class Main extends JavaPlugin {
 		System.out.println("========== EditSign ==========");
 		System.out.println(" ");
 		System.out.println("Author: GibMirRechte");
-		System.out.println("Version: 1.0");
+		System.out.println("Version: " + Bukkit.getPluginManager().getPlugin("EditSign").getDescription().getVersion());
 		System.out.println("Discord: yhBX6KT");
 		System.out.println(" ");
 		System.out.println("========== EditSign ==========");
